2093f568fb24f218acb2222d7129b8012b0ff86f
Nishasahoo <Nisha.sahoo@vithiit.com>
2023-11-27 15:31:13 +0530

    footer added

--- src/Components/HomeComponents/Projects.jsx
+++ src/Components/HomeComponents/Projects.jsx
@@ -3,20 +3,16 @@ import React from 'react'
 
 
 
-const Projects = ({image,title}) => {
+const Projects = ({ image, title }) => {
   return (
     <div>
       <h2>Project</h2>
       <div className="flex items-center justify-start h-screen ml-8">
-  <div className="bg-blue-100 bg-opacity-75 rounded-[30px] w-[425px] h-[312px] p-8 text-[#192839] text-white font-semibold">
-    <Image src={image} height={450} width={500} className="rounded-[30px] mx-auto" />
-    <p className="text-center mt-4 text-[#192839] font-semibold text-lg">{title}</p>
-  </div>
-</div>
-
-
-
-
+        <div className="bg-blue-100 bg-opacity-75 rounded-[30px] w-[425px] h-[312px] p-8 text-[#192839]  font-semibold">
+          <Image src={image} height={450} width={500} className="rounded-[30px] mx-auto" />
+          <p className="text-center mt-4 text-[#192839] font-semibold text-lg">{title}</p>
+        </div>
+      </div>
 
 
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,10 @@
 import React from 'react'
 import Navbar from '@/Components/CommonComponents/Navbar'
 import HeroSection from '@/Components/HomeComponents/HeroSection';
-
+import Projects from '@/Components/HomeComponents/Projects';
+import VideoSection from '@/Components/VideoSection';
+import CtaButton from '@/Components/CtaButton';
+import Footer from '@/Components/Footer';
 
 
 const navbarData = {
@@ -26,6 +29,9 @@ const navbarData = {
       button: "Login"
     }
 
+   
+
+
 const handleButtonClick = () => {
 
   console.log('Button clicked!');
@@ -37,12 +43,26 @@ const handleButtonHeroButtonClick = () => {
 
 const data = 
   {
-    image: "",
+    image: "https://res.cloudinary.com/dagmm478n/image/upload/v1700832675/INA%20SANNIDI/WhatsApp_Image_2023-11-24_at_6.59.57_PM_tc3crp.jpg",
     title: "upcoming products"
   }
 
- 
+ const VideoSectionData  =
+ {
+  imageUrl:"https://res.cloudinary.com/dagmm478n/image/upload/v1700833360/INA%20SANNIDI/Group_1000001900_zibnxf.png",
+  title: "Take a short tour on our Venturest",
+  // heading: "headingText",
+  description: "Welcome to your dream home at Sannidhi Group, where elegance meets modern comfort. This stunning property redefines the art of sophisticated living",
+  button: "Login"
+}
 
+const CtaButtonData =
+{
+  heading: "Contact",
+  title: "Connect with us and get more details on our exciting projects",
+  description: "Welcome to your dream home at Sannidhi Group, where elegance meets modern comfort. This stunning property redefines the art of sophisticated living. Connect and discover more on our ongoing and upcoming projects.",
+  button: "Discover More",
+}
 
 export default function Home() {
   return (
@@ -57,9 +77,25 @@ export default function Home() {
         onButtonClick={handleButtonHeroButtonClick}
         
       />
-
-
-
+    <Projects image={data.image} title={data.title}/> 
+    <VideoSection
+        image={VideoSectionData.imageUrl}
+        title={VideoSectionData.title}
+        description={VideoSectionData.description}
+        buttonLabel={VideoSectionData.button}
+        onButtonClick={handleButtonHeroButtonClick}
+        
+      />
+     <CtaButton
+        heading={CtaButtonData.heading}
+        title={CtaButtonData.title}
+        description={CtaButtonData.description}
+        buttonLabel={CtaButtonData.button}
+        onButtonClick={handleButtonHeroButtonClick}
+        
+      />
+   <Footer/>
+    
     </div>
   )
 }
