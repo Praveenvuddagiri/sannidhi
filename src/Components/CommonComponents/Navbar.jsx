1ef5658736910028f9b452c26d75e65b4bd9f308
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-11-27 11:44:47 +0530

    Navbar completed

--- src/Components/CommonComponents/Navbar.jsx
+++ src/Components/CommonComponents/Navbar.jsx
@@ -5,37 +5,49 @@ import React from "react";
 
 
 const Navbar = ({ logoSrc, navLinks, buttonLabel, onButtonClick }) => {
-    
+
 
   return (
     <div className="flex justify-center items-center">
-        <div className="flex items-center justify-between px-4 py-6 bg-white text-[#333] w-[85%]">
-      {logoSrc && <Image src={logoSrc} alt="Logo" className="h-[50px] w-auto" width={400} height={400} />}
+      <nav className="flex justify-evenly w-full">
+        <div className="flex items-center justify-between py-5 bg-white text-[#333] w-[92%]">
 
-      
-      <nav>
-        <ul className="flex items-center gap-12">
-          {navLinks &&
-            navLinks.map((link, index) => (
-              <li key={index}>
-                <Link href={link.url} className="hover:text-gray-300 text-[16px] font-[400]">
-                  {link.text}
-                </Link>
-              </li>
-            ))}
-        </ul>
+          <Link href="/">
+            <Image
+              src={logoSrc}
+              alt="logo"
+              width={400}
+              height={400}
+              className="w-24"
+            />
+          </Link>
+
+          <ul className="flex items-center justify-between w-[70%]">
+            {navLinks &&
+              navLinks.map((link, index) => (
+                <li key={index}>
+                  <Link href={link.url} className="hover:text-gray-300 text-[16px] font-[400]">
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+
+            <div className="w-fit">
+              {buttonLabel && onButtonClick && (
+                <button
+                  onClick={onButtonClick}
+                  className="text-[#3A7CBA] text-[18px] font-[600] px-6 py-2 rounded-sm border border-blue-500 w-fit"
+                >
+                  {buttonLabel}
+                </button>
+              )}
+            </div>
+          </ul>
+
+        </div>
       </nav>
-      {buttonLabel && onButtonClick && (
-        <button
-          onClick={onButtonClick}
-          className="text-[#3A7CBA] text-[18px] font-[600] px-4 py-2 rounded-sm border border-blue-500"
-        >
-          {buttonLabel}
-        </button>
-      )}
     </div>
-    </div>
-    
+
   );
 };
 
