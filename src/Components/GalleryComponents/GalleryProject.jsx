2b3560f7ef1e9e37569d7c391839aa11a15e908d
Ramesh <ramesh.gorekanti@gmail.com>
2023-11-29 18:29:37 +0530

    changes

--- src/Components/GalleryComponents/GalleryProject.jsx
+++ src/Components/GalleryComponents/GalleryProject.jsx
@@ -1,35 +1,24 @@
-import React from 'react';
-import GalleryCard from './GalleryCard';
-import CarouselCard from '../SwiperCarousl/CarouslCard';
-
+import React from "react";
+import GalleryCard from "./GalleryCard";
+import CarouselCard from "../SwiperCarousl/CarouslCard";
 
 const GalleryProject = () => {
+  const GalleryProjectData = {
+    heading: "",
+  };
+ 
+  return (
+    <div>
+      <div className="flex justify-center items-center flex-col mt-16 mb-5 text-xl">
+        <h2 className="text-4xl text-[#3A7CBA]">Gallery</h2>
+      </div>
 
-    const GalleryProjectData =
-    {
-        heading: "",
-    }
-
-
-    return (
-        <div>
-            <div className='flex justify-center items-center flex-col mt-16 mb-5 text-xl'><h2 className='text-4xl text-[#3A7CBA]'>Gallery</h2>
-
-            </div>
-
-            <div className='flex flex-wrap gap-4 justify-center'>
-                
-
-                    <GalleryCard
-                        heading={GalleryProjectData.heading}
-
-                    />
-                   <CarouselCard/>
-
-            </div>
-        </div>
-
-    );
+      <div className="flex flex-wrap gap-4 justify-center">
+        <GalleryCard heading={GalleryProjectData.heading} />
+        <CarouselCard />
+      </div>
+    </div>
+  );
 };
 
 export default GalleryProject;
--- src/app/projectdetails/page.jsx
+++ src/app/projectdetails/page.jsx
@@ -70,7 +70,7 @@ const page = () => {
       />
        
       <GalleryProject/>
-      <LocationProject/>
+      <LocationProject data={LocationProjectData}/>
     
       <VideoSection
         image={VideoSectionData.imageUrl}
