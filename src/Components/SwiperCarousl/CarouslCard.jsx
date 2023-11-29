fbccaafc12bbc9e8546c57d43c69ea355cc77f5b
Nishasahoo <Nisha.sahoo@vithiit.com>
2023-11-29 18:39:59 +0530

    card added

--- src/Components/SwiperCarousl/CarouslCard.jsx
+++ src/Components/SwiperCarousl/CarouslCard.jsx
@@ -7,27 +7,27 @@ const CarouselCard = () => {
     {
       id: "item-1",
       title: "Slide 1",
-      image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701167272/INA%20SANNIDI/4a1d841d0499a417231c1db0a6b27d35_tgxl1d.png", // Replace with your image URL
+      image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701155051/INA%20SANNIDI/14b800e5bdbd38e838ff76d9b76b7a7f_yv1i3i.png", // Replace with your image URL
     },
     {
       id: "item-2",
       title: "Slide 2",
-      image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701167272/INA%20SANNIDI/4a1d841d0499a417231c1db0a6b27d35_tgxl1d.png", // Replace with your image URL
+      image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701155051/INA%20SANNIDI/14b800e5bdbd38e838ff76d9b76b7a7f_yv1i3i.png", // Replace with your image URL
     },
     {
       id: "item-3",
       title: "Slide 3",
-      image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701167272/INA%20SANNIDI/4a1d841d0499a417231c1db0a6b27d35_tgxl1d.png", // Replace with your image URL
+      image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701155051/INA%20SANNIDI/14b800e5bdbd38e838ff76d9b76b7a7f_yv1i3i.png", // Replace with your image URL
     },
     {
         id: "item-4",
         title: "Slide 4",
-        image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701167272/INA%20SANNIDI/4a1d841d0499a417231c1db0a6b27d35_tgxl1d.png", // Replace with your image URL
+        image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701155051/INA%20SANNIDI/14b800e5bdbd38e838ff76d9b76b7a7f_yv1i3i.png", // Replace with your image URL
     },
     {
         id: "item-5",
         title: "Slide 5",
-        image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701167272/INA%20SANNIDI/4a1d841d0499a417231c1db0a6b27d35_tgxl1d.png", // Replace with your image URL
+        image: "https://res.cloudinary.com/dagmm478n/image/upload/v1701155051/INA%20SANNIDI/14b800e5bdbd38e838ff76d9b76b7a7f_yv1i3i.png", // Replace with your image URL
     },
   ];
   const [currentSlide, setCurrentSlide] = useState(mockItems[0].id);
@@ -37,6 +37,7 @@ const CarouselCard = () => {
     slideToPrevItem,
     slideToNextItem,
     useListenToCustomEvent,
+    slideToItem, // Added this function to enable sliding by clicking on dots
   } = useSpringCarousel({
     itemsPerSlide: 5,
     withLoop: true,
@@ -47,19 +48,18 @@ const CarouselCard = () => {
         ...item,
         renderItem: (
           <div
-            className={`grid aspect-[2] w-full place-items-center text-black transition-all duration-700  ${
-              currentSlide === item.id
-                ? "z-10 scale-150"
-                : ""
-            }`}
+            key={item.id}
+            className={`grid aspect-[2] w-full place-items-center text-black transition-all duration-700 ${currentSlide === item.id
+              ? "z-10 scale-150"
+              : ""}`}
           >
             <img
               src={item.image}
               alt={item.title}
-              className="h-full w-full object-cover rounded-xl"
+              className="h-full w-full object-cover rounded-xl bg-cover"
             />
-            <p className="mt-2 text-lg text-center text-black ">
-              {item.title} 
+            <p className="mt-2 mb-6 text-lg text-center text-black ">
+              {item.title}
             </p>
           </div>
         ),
@@ -74,48 +74,21 @@ const CarouselCard = () => {
   });
 
   return (
-    <div className="py-20 relative bg-[#EAEAEA]">
-      <button
-        onClick={slideToPrevItem}
-        className="absolute top-1/2 -translate-y-1/2 -translate-x-full left-[10%]"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-      </button>
+    <div className="py-10 relative overflow-hidden bg-[#EAEAEA]">
       <div className="mx-auto w-[80%] overflow-x-clip py-[8%] relative">
         {carouselFragment}
+        <div className="flex items-center justify-center space-x-2 mt-4">
+          {mockItems.map((item) => (
+            <button
+              key={item.id}
+              onClick={() => slideToItem(item.id)}
+              className={`h-2 w-2 bg-blue-500 rounded-full focus:outline-none ${
+                currentSlide === item.id ? 'opacity-75' : 'opacity-50'
+              }`}
+            />
+          ))}
+        </div>
       </div>
-      <button
-        onClick={slideToNextItem}
-        className="absolute top-1/2 -translate-y-1/2 translate-x-full right-[10%]"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5"
-          />
-        </svg>
-      </button>
     </div>
   );
 };
